1500c0fc9365c81c6958bf8f55dff6771a3cf12c
one <esport.py.m45@gmail.com>
2024-03-22 23:28:36 +0530

    levelorder traversal question done

--- 8.Tree/levelOrderTraversal.cpp
+++ 8.Tree/levelOrderTraversal.cpp
@@ -1,49 +1,24 @@
-#include <iostream>
-#include <queue>
-using namespace std;
 
-struct node{
-    int key;
-    node *left;
-    node *right ;
-    node(int x){
-        key=x;
-        left= NULL;
-        right = NULL;
-    };
-};
-
-void levelOrderTraversal(node *root){
-    if(root == NULL) return;
-    queue <node*> q;
-    q.push(root);
-    q.push(NULL);
-    int size =0;
-    while( q.size()>1){
-        node* curr =q.front();
-        if(curr!=NULL) size ++;
-        q.pop();
-        if(curr ==NULL){
-            cout << endl;
-            q.push(NULL);
-            continue;
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        if (root ==NULL) return ans;
+        queue <TreeNode *> q;
+        q.push(root);
+        while(!q.empty()){
+            int s= q.size();
+            vector <int> v;
+            for(int i=0;i<s;i++){
+                TreeNode* curr=q.front();
+                q.pop();
+                if(curr->left!=NULL)q.push(curr->left);
+                if(curr->right!=NULL)q.push(curr->right);
+                
+                v.push_back(curr->val);
+            }
+            ans.push_back(v);
         }
-        cout << curr->key << " ";
-        if(curr->left != NULL) q.push(curr->left);
-        if(curr->right != NULL) q.push(curr->right);
+        return ans;
     }
-    cout << endl;
-    cout << "size : " << size;
 };
-
-int main(){
-    node *root = new node(10);
-    root->left = new node(20);
-    root->left->left =new node(40);
-    root->left->left->left =new node(70);
-    root->right = new node(30);
-    root->right->left = new node(50);
-    root->right->right =new node(60);
-    levelOrderTraversal(root);
-    return 0;
-}
