2912730a38ef1bd45d4504970880997f8bfafe13
one <esport.py.m45@gmail.com>
2024-03-27 14:54:46 +0530

    diameter of binary tree done

--- 8.Tree/diameter.cpp
+++ 8.Tree/diameter.cpp
@@ -1,36 +1,16 @@
-#include <iostream>
-using namespace std;
-
-struct node{
-    int key;
-    node *left;
-    node *right ;
-    node(int x){
-        key=x;
-        left= NULL;
-        right = NULL;
-    };
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter =0;
+        height(root, diameter);
+        return diameter;
+    }
+private:
+    int height(TreeNode* root,int &res) {
+        if (root == NULL) return 0;
+        int lh= height(root->left);
+        int rh= height(root->right);
+        res= max(res,lh+rh);
+        return 1+max(rh,lh);
+    }
 };
-int res=0;
-int diameter(node *root){
-    if(root == NULL) return 0;
-    int lh =diameter(root->left);
-    
-    int rh =diameter(root->right);
-    res=max(res,1+rh+lh);
-    
-    return 1+max(rh,lh);
-};
-
-int main(){
-    node *root = new node(10);
-    root->left = new node(20);
-    root->left->left =new node(40);
-    root->left->left->left =new node(70);
-    root->right = new node(30);
-    root->right->left = new node(50);
-    root->right->right =new node(60);
-    diameter(root);
-    cout << res <<endl;
-    return 0;
-}
